ad6f2c4437fa9b9b37f62bff3b74521d3bdb7ca9
Moriquende <97157285+Moriquende@users.noreply.github.com>
2022-07-15 15:14:14 +0400

    Update isPalindrome.java

--- isPalindrome.java
+++ isPalindrome.java
@@ -1,9 +1,7 @@
-public class Solution {
-  public static Boolean isPalindrome(String text) {
-      if(text.length() <= 1) {
-        return true;
-      }
-      return (text.charAt(0) == text.charAt(text.length()-1)) && 
-          isPalindrome(text.substring(1, text.length()-1));
-    }
+public static Boolean isPalindrome(String text) {
+  if(text.length() <= 1) {
+    return true;
   }
+  return (text.charAt(0) == text.charAt(text.length()-1)) && 
+    isPalindrome(text.substring(1, text.length()-1));
+}
