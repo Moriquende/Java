7b8d293fb2ce4d0ed36b7c26135e8e23470b0ad9
Moriquende <97157285+Moriquende@users.noreply.github.com>
2022-07-15 15:16:38 +0400

    Create isProperly.java

---
+++ isProperly.java
@@ -0,0 +1,15 @@
+public static Boolean isProperly(String sequence) {
+		Stack<Character> st = new Stack<>();
+		for(int i = 0; i < sequence.length(); i++) {
+			if(sequence.charAt(i) == '(') {
+				st.add(sequence.charAt(i));
+			} else {
+				if(!st.isEmpty()) {
+					st.pop();
+				} else {
+					return false;
+				}
+			}
+		}
+  return st.isEmpty();
+}
